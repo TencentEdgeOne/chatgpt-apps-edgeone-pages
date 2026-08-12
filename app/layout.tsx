f5e03b1cb44fd5034c848aa5b244b91c5a0df73c
edgeone pages <simxin@tencent.com>
2026-08-12 19:53:23 +0800

    update meta info

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'ChatGPT Apps EdgeOne Pages Example',
-  description: 'A Next.js app using Model Context Protocol (MCP)',
+  title: 'ChatGPT Apps EdgeOne Pages Example | EdgeOne Makers',
+  description: 'A Next.js app using Model Context Protocol (MCP) · Demo only · EdgeOne Makers',
+  keywords: "EdgeOne Makers, Demo only",
 }
 
 export default function RootLayout({
